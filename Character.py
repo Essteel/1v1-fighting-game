99c2273f881bf4c52b03749aad48f8d2b1dfaa4e
Essteel <siannon.steel@gmail.com>
2022-09-18 15:31:33 +0930

    Added get_character_stats function

--- Character.py
+++ Character.py
@@ -1,8 +1,8 @@
 class Character:
     def __init__(self, name, max_hp, hp, basic_attack, special_attack):
-        name.self = name
-        name.max_hp = max_hp
-        name.hp = hp
-        name.basic_attack = basic_attack
-        name.special_attack = special_attack
+        self.name = name
+        self.max_hp = max_hp
+        self.hp = hp
+        self.basic_attack = basic_attack
+        self.special_attack = special_attack
         
