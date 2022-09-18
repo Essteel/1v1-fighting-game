2dbce3ee7f4e43e727b0f5862e32755b0fb3cc1d
Essteel <siannon.steel@gmail.com>
2022-09-18 14:19:33 +0930

    Created character class

---
+++ Character.py
@@ -0,0 +1,8 @@
+class Character:
+    def __init__(self, name, max_hp, hp, basic_attack, special_attack):
+        name.self = name
+        name.max_hp = max_hp
+        name.hp = hp
+        name.basic_attack = basic_attack
+        name.special_attack = special_attack
+        
