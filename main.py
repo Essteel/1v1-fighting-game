66e1579374fd7b6a2e42a593a7ab5080010d13dd
Essteel <siannon.steel@gmail.com>
2022-09-18 14:38:54 +0930

    Created four characters

---
+++ main.py
@@ -0,0 +1,6 @@
+import Character
+
+bella_brawler = Character.Character('Bella Brawler', 350, 350, 30, 50)
+monty_mischief = Character.Character('Monty Mischief', 200, 200, 40, 70)
+sayo_swift = Character.Character('Sayo Swift', 200, 200, 50, 60)
+dave_danger = Character.Character('Dave Danger', 175, 175, 50, 80)
