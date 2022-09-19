fbe99dc135d479440dd2abb449f763a6b32473f3
Essteel <siannon.steel@gmail.com>
2022-09-19 16:01:04 +0930

    Moved get char stats function, added comments & pseudocode to Character.py for assign Character to Player function

--- Character.py
+++ Character.py
@@ -5,4 +5,25 @@ class Character:
         self.hp = hp
         self.basic_attack = basic_attack
         self.special_attack = special_attack
-        
+
+# function that will allow user to view a list of characters and their stats
+def get_character_stats():
+    user_input = input('View character stats (y/n): ').lower()
+    if user_input == 'y':
+        return print(f'Please pick a character:\n{bella_brawler.__dict__}\n{monty_mischief.__dict__}\n{sayo_swift.__dict__}\n{dave_danger.__dict__}')
+
+# sub class for player character
+class Player(Character):
+    super().__init__()
+
+# function to assign a character to the player
+def select_player(choice):
+    print('Please choose a character:\n 1 = Bella\n 2 = Monty\n 3 = Sayo\n 4 = Dave\n')
+    if choice == '1':
+        pass # set character as Bella
+    elif choice == '2':
+        pass # set character as Monty
+    elif choice == '3':
+        pass # set character as Sayo
+    else:
+        pass # set character as Dave
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import Character
 
+# Create character objects
 bella_brawler = Character.Character('Bella Brawler', 350, 350, 30, 50)
 monty_mischief = Character.Character('Monty Mischief', 200, 200, 40, 70)
 sayo_swift = Character.Character('Sayo Swift', 200, 200, 50, 60)
 dave_danger = Character.Character('Dave Danger', 175, 175, 50, 80)
 
-# function that will allow user to view a list of characters and their stats
-def get_character_stats():
-    user_input = input('View character stats (y/n): ').lower()
-    if user_input == 'y':
-        return print(f'Please pick a character:\n{bella_brawler.__dict__}\n{monty_mischief.__dict__}\n{sayo_swift.__dict__}\n{dave_danger.__dict__}')
-
-get_character_stats()
+Character.get_character_stats()
